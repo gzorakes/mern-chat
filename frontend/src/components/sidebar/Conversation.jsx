35b496c81c3cc9b69e508efa62fc23ba6b607e15
zorakisgeorge <zorakisgeorge@gmail.com>
2025-09-23 18:20:03 +0300

    Home page UI design

---
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,26 @@
+function Conversation() {
+  return (
+    <>
+      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+            <img
+              src="https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes/128/user-male-circle-blue-512.png"
+              alt="user avatar"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-200">John Doe</p>
+            <span className="text-xl">🙃</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="divider my-0 py-0 h-1" />
+    </>
+  );
+}
+
+export default Conversation;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,13 @@
+import MessageContainer from "../../components/messages/MessageContainer";
+import Sidebar from "../../components/sidebar/Sidebar";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden shadow-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10">
+      <Sidebar />
+      <MessageContainer />
+    </div>
+  );
 }
 
 export default Home;
