b51ac77dc80ecac384ef6432167939c15b3befcc
zorakisgeorge <zorakisgeorge@gmail.com>
2025-09-24 00:11:39 +0300

    Get conversations - zustand

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -2,17 +2,25 @@ import { TiMessages } from "react-icons/ti";
 
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
+import useConversation from "../../zustand/useConversation";
+import { useEffect } from "react";
 
 function MessageContainer() {
-  const noChatSelected = false;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+    // cleanup function (unmounts)
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
+
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
           <div className="bg-slate-500 px-4 py-2 mb-2">
-            <span className="label-text">To:</span> <span className="text-white font-bold">John Doe</span>
+            <span className="label-text">To:</span> <span className="text-white font-bold">{selectedConversation.fullName}</span>
           </div>
 
           <Messages />
