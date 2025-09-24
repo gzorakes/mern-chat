b09fdff12c3366ca5f32e6b420e0420374a08313
zorakisgeorge <zorakisgeorge@gmail.com>
2025-09-24 11:18:44 +0300

    Send message functionality

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,16 +1,25 @@
-function Message() {
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
+
+function Message({ message }) {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const formattedTime = extractTime(message.createdAt);
+  const fromMe = message.senderId === authUser._id;
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img
-            src="https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes/128/user-male-circle-blue-512.png"
-            alt="Tailwind CSS chat bubble component"
-          />
+          <img src={profilePic} alt="Tailwind CSS chat bubble component" />
         </div>
       </div>
-      <div className={`chat-bubble text-white bg-blue-500`}>Hi! What is upp?</div>
-      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">12:42</div>
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>{message.message}</div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{formattedTime}</div>
     </div>
   );
 }
